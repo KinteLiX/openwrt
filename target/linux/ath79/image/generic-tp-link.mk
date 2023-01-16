9a0e319acfd4e87ba83e7ae921a05918522f2091
Foica David <superh552@gmail.com>
2023-01-16 23:08:12 +0200

    P9 support

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -503,6 +503,18 @@ define Device/tplink_deco-m4r-v1
 endef
 TARGET_DEVICES += tplink_deco-m4r-v1
 
+define Device/tplink_deco-p9-v1
+  $(Device/tplink-safeloader-uimage)
+  SOC := qca9563
+  IMAGE_SIZE := 13824k
+  DEVICE_MODEL := Deco P9
+  DEVICE_VARIANT := v1
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct
+  SUPPORTED_DEVICES += deco-p9-v1
+  TPLINK_BOARD_ID := DECO-P9-V1
+endef
+TARGET_DEVICES += tplink_deco-p9-v1
+
 define Device/tplink_deco-s4-v2
   $(Device/tplink-safeloader-uimage)
   SOC := qca9563
